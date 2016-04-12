8f1d46d718a449f49738b6983d76c5370e4cd868
David Hallum <david@kenzielane.com>
2016-04-12 10:43:12 -0400

    Adding syslog input for debugging help

--- start_logstash.sh
+++ start_logstash.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i "s/ELS_HOST/$ELS_HOST/" /conf/gelf.conf
+sed -i "s/ELS_HOST/$ELS_HOST/" /conf/logstash.conf
 
-logstash --verbose --debug -f /conf/gelf.conf
+logstash --verbose --debug -f /conf/logstash.conf
